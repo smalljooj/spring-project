270be181f61aa17114ce09cf70141e858a8b2112
smalljooj <firespin147@gmail.com>
2023-07-12 15:26:27 -0300

    repositories

---
+++ src/main/java/com/ismae/project/repositories/TaskRepository.java
@@ -0,0 +1,24 @@
+package com.ismae.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+
+import com.ismae.project.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>
+{
+
+	List<Task> findByUser_Id(Long id);
+	/*
+
+	@Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+	List<Task> findByUser_Id(@Param("id") Long id);
+
+	@Query(value = "SELECT * FROM tasks t WHERE t.user_id = :id", nativeQuery = true)
+	List<Task> findByUser_Id(@Param("id") Long id);
+	*/
+}
---
+++ src/main/java/com/ismae/project/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ismae.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ismae.project.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{ }
