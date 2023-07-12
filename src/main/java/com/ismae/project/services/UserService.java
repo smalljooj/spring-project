a70b990786b696655856707d617a76a6131fb525
smalljooj <firespin147@gmail.com>
2023-07-12 16:27:33 -0300

    user services

---
+++ src/main/java/com/ismae/project/services/UserService.java
@@ -0,0 +1,62 @@
+package com.ismae.project.services;
+
+import com.ismae.project.repositories.UserRepository;
+
+import java.util.Optional;
+
+import com.ismae.project.models.User;
+import com.ismae.project.repositories.TaskRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserService 
+{
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private TaskRepository taskRepository;
+
+
+	public User findById(Long id)
+	{
+		Optional<User> user = this.userRepository.findById(id);
+		return user.orElseThrow(() -> new RuntimeException("Usuario nao encontrado: id i " + id + "."));
+	}
+
+	@Transactional 
+	public User createUser(User obj)
+	{
+		obj.setId(null);
+		obj = this.userRepository.save(obj);
+		this.taskRepository.saveAll(obj.getTasks());
+		return obj;
+	}
+
+	@Transactional 
+	public User updateUser(User obj)
+	{
+		User newObj = findById(obj.getId());
+		newObj.setPassword(obj.getPassword());
+		return this.userRepository.save(newObj);
+	}
+
+	@Transactional
+	public void delete(Long id)
+	{
+		findById(id);
+		try 
+		{
+			this.userRepository.deleteById(id);	
+		}
+		catch(Exception e) 
+		{
+			throw new RuntimeException("Erro ao deletar o usuario, pois ha entidades relacionadas");
+		}
+	}
+
+
+}
